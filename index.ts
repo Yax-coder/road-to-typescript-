29472a2d2c225f394795874fd06e47551701b546
yahya <ahmadyahya1800@gmail.com>
2022-07-24 15:56:08 +0100

    Interface

--- index.ts
+++ index.ts
@@ -2,6 +2,21 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+    The Todo with ID: ${id}
+    Has a Title of: ${title}
+    Is it completed: ${completed}
+  `);
 });
