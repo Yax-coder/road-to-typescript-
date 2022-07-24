a242174861c8980f797b190bf0a1165172aa6ef0
yahya <ahmadyahya1800@gmail.com>
2022-07-24 16:52:06 +0100

    types for arrays, classes and object literals

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -8,3 +8,21 @@ let nothing: undefined = undefined;
 // built in objects
 
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green", "blue"];
+let myNunbers: number[] = [1, 2, 3, 4, 5];
+let truths: boolean[] = [true, false];
+
+// classes
+
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
