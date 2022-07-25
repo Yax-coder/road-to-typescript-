96bbec17092dedba42511f393bc42576585fb2de
yahya <ahmadyahya1800@gmail.com>
2022-07-25 15:47:56 +0100

    Other function 'returns types'

--- annotations/Functions.ts
+++ annotations/Functions.ts
@@ -1,3 +1,17 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
